1728037319c6b0345dc8cdc58289da8d47a6d68e
liming998 <150116108+liming998@users.noreply.github.com>
2024-11-11 07:01:15 +0800

    Add files via upload

---
+++ 10/scene/title/scene_title.js
@@ -0,0 +1,27 @@
+class SceneTitle extends GuaScene {
+    constructor(game) {
+        super(game);
+        var label = GuaLabel.new(game, 'hello form gua')
+        this.addElement(label)
+
+        // cave bg
+        var cave = GuaImage.new(game, 'cave')
+        this.addElement(cave)
+        // player
+        var w = GuaAnimation.new(game)
+        w.x = 100
+        w.y = 180
+        this.w = w
+        this.addElement(w)
+        this.setupInputs()
+    }
+    setupInputs() {
+        var self = this
+        self.game.registerAction('a', function (keyStatus){
+            self.w.move(-2, keyStatus)
+        })
+        self.game.registerAction('d', function (keyStatus){
+            self.w.move(2, keyStatus)
+        })
+    }
+}
